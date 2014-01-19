db03c54d71cd0cb962c20360f173dad48255663b
Raphael de Andrade <rhdeandrade@gmail.com>
2014-01-19 20:02:02 -0200

    Generated barcodes

--- app/models/product.rb
+++ app/models/product.rb
@@ -43,4 +43,5 @@ class Product < ActiveRecord::Base
     end
     status
   end
+
 end
--- app/models/product_base.rb
+++ app/models/product_base.rb
@@ -1,4 +1,9 @@
 class ProductBase < ActiveRecord::Base
+
+  require 'barby'
+  require 'barby/outputter/rmagick_outputter'
+  require 'barby/barcode/code_128' 
+
   attr_accessible :name, :code, :price
   
   has_many :products, dependent: :destroy
@@ -6,4 +11,20 @@ class ProductBase < ActiveRecord::Base
   validates_presence_of :name, :code, :price
 
   validates_uniqueness_of :code
+
+  # puts Barby::Code128B::CODEA.encoding
+  # barcode = Barby::Code128B.new('123')
+  # File.open('code128B.gif', 'wb'){|f|
+  #   f.write barcode.to_gif
+  # }
+  def barcode
+    barcode = Barby::Code128B.new("#{self.code}")
+    file_path = "public/barcodes/#{self.code}.gif"
+    file_web = "/barcodes/#{self.code}.gif"
+    File.open(file_path, 'wb'){|f|
+      f.write barcode.to_gif
+    }
+
+    file_web
+  end
 end
